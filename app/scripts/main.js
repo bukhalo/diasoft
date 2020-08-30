a6faaad666266a04b12e538d12cb77ea43d8b106
Aleksandr Bukhalo <a@bukhalo.com>
2020-08-30 12:47:58 +0300

    feat: save forms state in localstorage, read forms state on page load

---
+++ app/scripts/main.js
@@ -0,0 +1,49 @@
+const initForms = () => {
+  // All forms on page
+  const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const formInputs = form.querySelectorAll('input');
+
+    // Submit form handling
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const form = event.target;
+
+      const formInputs = form.querySelectorAll('input');
+      formInputs.forEach((input) => {
+        localStorage.setItem(
+          `form-${form.id}-input-${input.name}-value`,
+          input.value,
+        );
+      });
+    });
+
+    // Clear form handling
+    const formClearButton = form.querySelector('.form__action-clear');
+    formClearButton.addEventListener('click', () => {
+      form.reset();
+      formInputs.forEach((input) => {
+        localStorage.removeItem(`form-${form.id}-input-${input.name}-value`);
+      });
+    });
+
+    // Read form state from localstorage
+    formInputs.forEach((input) => {
+      const inputState = localStorage.getItem(
+        `form-${form.id}-input-${input.name}-value`,
+      );
+      if (inputState) {
+        input.value = inputState;
+      }
+    });
+  });
+};
+
+/**
+ * Make some work on 'DOMContentLoaded' event
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  initForms();
+});
